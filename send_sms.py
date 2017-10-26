4d9e94c57b17d0f265f2472fe7548da489c7fb17
Rafael Cascalho <cascalhoeng@gmail.com>
2017-10-26 20:39:58 -0200

    Uploading Files

---
+++ send_sms.py
@@ -0,0 +1,10 @@
+from twilio.rest import Client
+from credentials import account_sid, auth_token, my_cell, my_twilio
+
+client = Client(account_sid, auth_token)
+
+str = '''
+txt msg ::: Oi Humano! Eu sou um bot do twilio, pode confiar em mim ;)
+'''
+
+message = client.messages.create(to = my_cell, from_ = my_twilio, body = str)
