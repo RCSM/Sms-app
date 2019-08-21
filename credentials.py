993918bc6de3566db7e9ee4cb9ecceee118ec8d5
Rafael Cascalho <cascalhoeng@gmail.com>
2019-08-21 14:32:32 -0300

    Applied the prettier plugin

--- credentials.py
+++ credentials.py
@@ -1,4 +1,7 @@
-account_sid = 'Sid da conta'
-auth_token = 'Token da conta'
-my_cell = 'Número do celular'
-my_twilio = 'Número do Twilio'
+account_sid = "Sid da conta"
+
+auth_token = "Token da conta"
+
+my_cell = "Número do celular"
+
+my_twilio = "Número do Twilio"
